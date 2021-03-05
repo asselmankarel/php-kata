cb4a3edf878ebd48d516b5a1a3b45aebac28386c
Karel Asselman <asselman.karel@gmail.com>
2021-03-05 14:35:51 +0100

    created test and class files for StringCalculator

---
+++ test/StringCalculatorTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use App\StringCalculator;
+
+class StringCalculatorTest extends TestCase
+{
+
+  /** @test */
+  function it_shoud_()
+  {
+
+  }
+
+  /** @test */
+  function it_shoud_2()
+  {
+    
+  }
+      /** @test */
+  function it_shoud_3()
+  {
+    
+  }
+
+}
