60f8046e39b85f03b0533582fa5732e20d6beb58
Karel Asselman <asselman.karel@gmail.com>
2021-03-05 14:31:50 +0100

    tests implemented and class refactored

--- src/BowlingGame.php
+++ src/BowlingGame.php
@@ -1,6 +1,68 @@
 <?php
 
+namespace App;
+
 class BowlingGame
 {
-  
+    private $rolls = [];
+    const FRAMES_PER_GAME = 10;
+
+
+    public function roll(int $pins)
+    {
+      $this->rolls[] = $pins;
+    }
+
+    public function score()
+    {
+      $score = 0;
+      $roll = 0;
+
+      foreach(range(1, self::FRAMES_PER_GAME) as $frame)
+      {
+        if($this->isStrike($roll))
+        {
+          $score += $this->pinCount($roll) + $this->strikeBonus($roll);
+          $roll += 1;
+          continue;
+        } 
+
+        $score += $this->defaultScore($roll);
+
+        if($this->isSpare($roll))
+        {
+          $score += $this->pinCount($roll + 2);
+        }
+
+        $roll += 2;
+      }
+
+      return $score;
+    }
+
+    protected function isSpare(int $roll): bool
+    {
+      return ($this->pinCount($roll) + $this->pinCount($roll + 1)) === 10;
+    }
+
+    protected function isStrike(int $roll): bool
+    {
+      return $this->pinCount($roll) === 10;
+    }
+
+    protected function defaultScore($roll): int
+    {
+      return $this->pinCount($roll) + $this->pinCount($roll + 1);
+    }
+
+    protected function pinCount(int $roll): int
+    {
+      return $this->rolls[$roll];
+    }
+
+    protected function strikeBonus(int $roll): int
+    {
+      return $this->pinCount($roll + 1) + $this->pinCount($roll + 2);
+    }
+
 }
--- test/BowlingGameTest.php
+++ test/BowlingGameTest.php
@@ -9,6 +9,110 @@ class BowlingGameTest extends TestCase
   /** @test */
   function it_scores_a_gutter_game_as_zero() 
   {
+    $game = new BowlingGame();
 
+    foreach(range(1, 20) as $roll)
+    {
+      $game->roll(0);
+    }
+
+    $this->assertSame(0, $game->score());
   }
+
+    /** @test */
+    function it_scores_all_ones() 
+    {
+      $game = new BowlingGame();
+  
+      foreach(range(1, 20) as $roll)
+      {
+        $game->roll(1);
+      }
+  
+      $this->assertSame(20, $game->score());
+    }
+
+     /** @test */
+     function it_awards_a_bonus_roll_for_every_spare() 
+     {
+       $game = new BowlingGame();
+       $game->roll(5);
+       $game->roll(5);
+
+       $game->roll(8);
+
+   
+       foreach(range(1, 17) as $roll)
+       {
+         $game->roll(0);
+       }
+   
+       $this->assertSame(26, $game->score());
+     }
+
+     function it_awards_2_bonus_roll_for_every_strike() 
+     {
+       $game = new BowlingGame();
+       $game->roll(10); //strike
+
+       $game->roll(5);
+       $game->roll(2);
+   
+       foreach(range(1, 16) as $roll)
+       {
+         $game->roll(0);
+       }
+   
+       $this->assertSame(24, $game->score());
+     }
+
+      /** @test */
+      function it_awards_an_extra_ball_for_spare_on_last_frame() 
+      {
+        $game = new BowlingGame();
+        
+        foreach(range(1, 18) as $roll)
+        {
+          $game->roll(0);
+        }
+
+        $game->roll(5);
+        $game->roll(5);
+ 
+        $game->roll(5);
+    
+        $this->assertSame(15, $game->score());
+      }
+
+      /** @test */
+      function it_awards_an_extra_ball_for_strike_on_last_frame() 
+      {
+        $game = new BowlingGame();
+        
+        foreach(range(1, 18) as $roll)
+        {
+          $game->roll(0);
+        }
+
+        $game->roll(10);
+
+        $game->roll(10);
+        $game->roll(10);
+    
+        $this->assertSame(30, $game->score());
+      }
+
+      /** @test */
+      function it_scores_a_perfect_game()
+      {
+        $game = new BowlingGame();
+        
+        foreach(range(1, 12) as $roll)
+        {
+          $game->roll(10);
+        }
+    
+        $this->assertSame(300, $game->score());
+
+      }
 }
