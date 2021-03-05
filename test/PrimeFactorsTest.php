fee35e9bdb7ffa947eeadd8bb7be6e26effc7aca
Karel Asselman <asselman.karel@gmail.com>
2021-03-05 10:22:09 +0100

    setup composer + scripts for tests:phpunit and styling:php_codesniffer

---
+++ test/PrimeFactorsTest.php
@@ -0,0 +1,12 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class PrimeFactorsTest extends TestCase
+{
+  /** @test */
+  function it_generates_prime_factors_for_1()
+  {
+    $factors = new PrimeFactors();
+    $this->assertEquals([], $factors->generate(1));
+  }
+}
