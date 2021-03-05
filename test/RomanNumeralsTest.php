171a392b5eb30fe799279bbc936ffee22acf5b79
Karel Asselman <asselman.karel@gmail.com>
2021-03-05 12:33:16 +0100

    RomanNumerals class and test class

---
+++ test/RomanNumeralsTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use App\RomanNumerals;
+
+class RomanNumeralsTest extends testCase
+{
+
+  /** @test 
+   * @dataProvider checks
+  */
+  function it_should_genrerate_roman_numeral($number, $expexted)
+  {
+    
+    $this->assertEquals($expexted, RomanNumerals::generate($number));
+  }
+
+  function checks()
+  {
+    return [
+      [1, 'I'],
+      [2, 'II'],
+      [4, 'IV'],
+      [5, 'V'],
+      [19, 'XIX'],
+      [90, 'XC'],
+      [100, 'C'],
+      [400, 'CD'],
+      [501, 'DI'],
+      [3999, 'MMMCMXCIX']
+    ];
+  }
+}
