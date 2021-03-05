99a3ab6bd6d529e1b30d4cdc8f52d8b0ea13c2f7
Karel Asselman <asselman.karel@gmail.com>
2021-03-05 13:18:05 +0100

    create BowlingGame class and test class

---
+++ test/BowlingGameTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use App\BowlingGame;
+
+class BowlingGameTest extends TestCase
+{
+
+  /** @test */
+  function it_scores_a_gutter_game_as_zero() 
+  {
+
+  }
+}
