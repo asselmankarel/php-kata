4f3fb91209265b93270b72c6c801b8ef8ec76db6
Karel Asselman <asselman.karel@gmail.com>
2021-03-06 19:08:37 +0100

    TennisMatch: tests and class implemented

--- src/TennisMatch.php
+++ src/TennisMatch.php
@@ -1,9 +1,81 @@
 <?php
 
 namespace App;
-
+/** @group tennis */
 class TennisMatch
 {
+  protected $playerOnePoints = 0;
+  protected $playerTwoPoints = 0;
 
-  
+  public function score()
+  {
+    if ($this->hasWinner())
+    {
+      return 'Winner: ' . $this->leader();
+    }
+
+    if($this->isAdvantage())
+    {
+      return 'Advantage: ' . $this->leader();
+    }
+
+    if($this->isDeuce())
+    {
+      return 'deuce';
+    }
+
+    return sprintf("%s-%s", $this->pointsToTerm($this->playerOnePoints), $this->pointsToTerm($this->playerTwoPoints));  
+  }
+
+  public function pointToPlayerOne()
+  {
+    $this->playerOnePoints++;
+  }
+
+  public function pointToPlayerTwo()
+  {
+    $this->playerTwoPoints++;
+  }
+
+  private function pointsToTerm($score)
+  {
+    switch ($score)
+    {
+      case 0: return 'love';
+      case 1: return 'fifteen';
+      case 2: return 'tirthy';
+      case 3: return 'forty';
+
+    }
+  }
+
+  private function hasWinner(): bool
+  {
+    if($this->playerOnePoints > 3 && $this->playerOnePoints >= $this->playerTwoPoints + 2)
+    {
+      return true;
+    }
+
+    if($this->playerTwoPoints > 3 && $this->playerTwoPoints >= $this->playerOnePoints + 2)
+    {
+      return true;
+    }
+    return false;
+  }
+
+  private function leader(): string
+  {
+    return $this->playerOnepoint > 0 ? 'Player 1' : 'Player 2';
+  }
+
+  private function isDeuce()
+  {
+    return $this->playerOnePoints >= 3 && $this->playerOnePoints == $this->playerTwoPoints;
+  }
+
+  private function isAdvantage():bool
+  {
+    return $this->playerOnePoints >= 3 && ($this->playerTwoPoints >= $this->playerOnePoints || $this->playerOnePoints >= $this->playerTwoPoints);
+  }
+
 }
--- test/TennisMatchTest.php
+++ test/TennisMatchTest.php
@@ -6,7 +6,43 @@ use App\TennisMatch;
 class TennisMatchTest extends TestCase
 {
 
-  /** @test */
-  
+  /** @test 
+   * @dataProvider scores
+  */
+  function it_scores_a_tennis_match($playerOnePoints, $playerTwoPoints, $score)
+  {
+    $match = new TennisMatch();
+
+    for($i = 0; $playerOnePoints < $i; $i++)
+    {
+      $match->PointToPlayerOne;
+    }
+   
+    for($i = 0; $playerTwoPoints < $i; $i++)
+    {
+      $match->PointToPlayerTwo;
+    }
+
+    $this->assertEquals('love-love', $match->score());
+  }
+
+   public function scores()
+   {
+     return [
+       [0, 0, 'love-love'],
+       [1, 0, 'fifteen-love'],
+       [1, 1, 'fifteen-fifteen'],
+       [2, 0, 'tirthy-love'],
+       [3, 0, 'forty-love'],
+       [3, 3, 'deuce'],
+       [2, 0, 'tirthy-love'],
+       [4, 2, 'Winner: Player 1'],
+       [4, 4, 'deuce'],
+       [6, 6, 'deuce'],
+       [4, 3, 'Advantage: Player 1'],
+       [4, 5, 'Advantage: Player 2']
+       
+     ];
+   }
 
 }
