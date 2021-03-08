64b86d0f6a9905983c92a785c0f8b98781e3fd80
Karel Asselman <asselman.karel@gmail.com>
2021-03-08 09:10:53 +0100

    added FizzBuzz class and test class

---
+++ test/FizzBuzzTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use App\FissBuzz;
+
+class FizzBuzzTest extends TestCase
+{
+
+  /** @test */
+  function it_returns_fizz_for_multiples_of_three()
+  {
+    
+  }
+}
