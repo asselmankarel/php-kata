5de0ce9a3e6e39a4d5b208c188ce2b06c89386cb
Karel Asselman <asselman.karel@gmail.com>
2021-03-08 09:23:59 +0100

    wrote first test and implementation

--- test/FizzBuzzTest.php
+++ test/FizzBuzzTest.php
@@ -1,7 +1,7 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-use App\FissBuzz;
+use App\FizzBuzz;
 
 class FizzBuzzTest extends TestCase
 {
@@ -9,6 +9,28 @@ class FizzBuzzTest extends TestCase
   /** @test */
   function it_returns_fizz_for_multiples_of_three()
   {
-    
+    foreach([3,6,9,12] as $number)
+    {
+      $this->assertEquals('fizz', FizzBuzz::convert($number));
+    }
   }
+
+  /** @test */
+  function it_returns_buzz_for_multiples_of_five()
+  {
+    foreach([5,10,20,25] as $number)
+    {
+      $this->assertEquals('buzz', FizzBuzz::convert($number));
+    }
+  }
+
+   /** @test */
+   function it_returns_fizzbuzz_for_multiples_of_three_and_five()
+   {
+     foreach([15,30,45,60] as $number)
+     {
+       $this->assertEquals('fizzbuzz', FizzBuzz::convert($number));
+     }
+   }
+
 }
